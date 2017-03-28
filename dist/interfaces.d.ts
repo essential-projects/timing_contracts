69ed6a1b086bd719ed10e757beeab5e3a42f445c
5ebastianMeier <sebastian.meier@5minds.de>
2017-03-28 11:34:16 +0200

    update interfaces

--- dist/interfaces.d.ts
+++ dist/interfaces.d.ts
@@ -1,15 +1,25 @@
 import { IEntity } from '@process-engine-js/core_contracts';
 import { ExecutionContext } from '@process-engine-js/core_contracts';
+import * as schedule from 'node-schedule';
 export interface ITimingRule {
+    second: number;
+    minute: number;
+    hour: number;
+    date: number;
+    month: number;
+    year: number;
+    dayOfWeek: number;
 }
 export interface ITimingService {
     once(date: Date, eventName: string, context: ExecutionContext): Promise<string>;
+    periodic(rule: ITimingRule, eventName: string, context: ExecutionContext): Promise<string>;
     cron(cronString: string, eventName: string, context: ExecutionContext): Promise<string>;
     cancel(timerId: string, context: ExecutionContext): Promise<void>;
 }
+export declare type TimerValue = string | Date | schedule.RecurrenceRule;
 export interface ITimerEntity extends IEntity {
     timerType: TimerType;
-    timerValue: string;
+    timerValue: TimerValue;
     eventName: string;
     lastElapsed?: Date;
 }
