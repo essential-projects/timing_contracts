b59816551208948473e410b98016a872aa97ccfd
ElRaptorus <christian.werner@5minds.de>
2018-12-05 14:07:58 +0100

    :recycle: Make ITimerService functions synchronous

--- src/itimer_service.ts
+++ src/itimer_service.ts
@@ -3,7 +3,7 @@ import {TimerRule} from './timer_rule';
 import * as moment from 'moment';
 
 export interface ITimerService {
-  once(date: moment.Moment, eventName: string): Promise<string>;
-  periodic(rule: TimerRule, eventName: string): Promise<string>;
-  cancel(timerId: string): Promise<void>;
+  once(date: moment.Moment, eventName: string): string;
+  periodic(rule: TimerRule, eventName: string): string;
+  cancel(timerId: string): void;
 }
