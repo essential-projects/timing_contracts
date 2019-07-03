702882438512bddc04a5fe73960746817670addc
ElRaptorus <christian.werner@5minds.de>
2019-07-03 10:04:15 +0200

    :fire: Remove TimerRule type

--- src/timer.ts
+++ src/timer.ts
@@ -1,13 +1,12 @@
 import * as moment from 'moment';
 import {TimerType} from './timer_type';
-import {TimerRule} from './timer_rule';
 
 export class Timer {
 
   public id: string;
   public type: TimerType;
   public expirationDate: moment.Moment;
-  public rule: TimerRule;
+  public rule: string;
   public eventName: string;
   public lastElapsed: Date;
 
